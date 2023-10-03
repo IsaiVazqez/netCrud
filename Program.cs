96a08ca70eb7c90d766ea0e8cb7dab6d4685646d
IsaiVazqez <7yuvryi@gmail.com>
2023-10-03 02:00:37 -0600

    middleware

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
+using AspNetCoreRateLimit;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<TypePersonService>();
@@ -7,6 +9,15 @@ builder.Services.AddDbContext<ApplicationContext>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+builder.Services.Configure<IpRateLimitPolicies>(builder.Configuration.GetSection("IpRateLimitPolicies"));
+builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+builder.Services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
 var app = builder.Build();
 
 app.UseCors(policy => policy
@@ -21,6 +32,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseIpRateLimiting();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
